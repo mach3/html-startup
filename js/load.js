5f06996064662c5195fca63647b378ca94beb306
mach3 <mach3ss@gmail.com>
2012-06-11 01:41:10 +0900

    add og:description, update load.js

--- js/load.js
+++ js/load.js
@@ -1,24 +1,47 @@
+/*!
+ * load.js
+ * (load resources, before and after document ready)
+ *
+ * @require jquery.js head.js
+ * @author mach3ss
+ */
+
 (function($, undefined){
 
-	window.JSDIR = $("script:last").attr("src").replace(/\?.*?$/,"").replace(/[^/]*?$/, "");
+	window.JSDIR = $("script:last").attr("src").replace(/\?.*?$/,"").replace(/[^\/]*?$/, "");
 	window.JSAPPDIR = JSDIR + "app/";
 	window.IMGDIR = JSDIR + "../img";
 
-	$(function(){
-		var resources, callback;
-
-		resources = [
-			"init.js"
-		];
-		callback = function(){};
-
+	var load = function(resources){
 		head.js.call(
 			head,
 			$.map(resources, function(name){
 				return JSAPPDIR + name;
-			}),
-			callback
+			})
 		);
+	};
+
+	/**
+	 * load before
+	 */
+	(function(){
+		var resources = [
+		];
+		if(resources.length){
+			load(resources);
+		}
+	}());
+
+	/**
+	 * load on ready
+	 */
+	$(function(){
+		var resources = [
+			"init.js"
+		];
+		if(resources.length){
+			load(resources);
+		}
 	});
 
 }(jQuery));
